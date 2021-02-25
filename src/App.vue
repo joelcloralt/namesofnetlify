2a720a651495b16c042ff178da2a4c7955eda300
Joel Cloralt <joel.cloralt@gmail.com>
2021-02-25 13:55:49 -0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" alt="Vue logo" src="./assets/phil.jpg">
+    <HelloWorld msg="What would Phil name this?"/>
   </div>
 </template>
 
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+  width: 200px;
 }
 </style>
